545d83d788e051348a68a7b0357a85ef1bc177d5
wccch <137507230+wccch-wzz@users.noreply.github.com>
2026-09-07 21:51:08 +0800

    sync: Brix 1.1 brixcore/mod/modinfo/ForgeOldModMetadata.java

--- brix-src/com/brixcore/mod/modinfo/ForgeOldModMetadata.java
+++ brix-src/com/brixcore/mod/modinfo/ForgeOldModMetadata.java
@@ -1,21 +1,6 @@
 package com.brixcore.mod.modinfo;
 
-import com.brixcore.mod.LocalModFile;
-import com.brixcore.mod.ModLoaderType;
-import com.brixcore.mod.ModManager;
-import com.brixcore.util.StringUtils;
-import com.brixcore.util.gson.JsonUtils;
-import com.google.gson.JsonParseException;
 import com.google.gson.annotations.SerializedName;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.Files;
-import java.nio.file.LinkOption;
-import java.nio.file.Path;
-import java.util.List;
 
 /* JADX INFO: loaded from: classes10.dex */
 public final class ForgeOldModMetadata {
@@ -104,82 +89,13 @@ public final class ForgeOldModMetadata {
     /* JADX WARN: Bottom block not found for handler: all -> 0x0145 */
     /*
         Code decompiled incorrectly, please refer to instructions dump.
+        To view partially-correct add '--show-bad-code' argument
     */
-    public static LocalModFile fromFile(ModManager modManager, Path modFile, FileSystem fs) throws Throwable {
-        BufferedReader reader;
-        List<ForgeOldModMetadata> modList;
-        String authors;
-        Path mcmod = fs.getPath("mcmod.info", new String[0]);
-        if (Files.notExists(mcmod, new LinkOption[0])) {
-            throw new IOException("File " + modFile + " is not a Forge mod.");
-        }
-        reader = Files.newBufferedReader(mcmod);
-        try {
-            JsonReader jsonReader = new JsonReader(reader);
-            try {
-                JsonToken firstToken = jsonReader.peek();
-                if (firstToken == JsonToken.BEGIN_ARRAY) {
-                    modList = (List) JsonUtils.GSON.fromJson(jsonReader, JsonUtils.listTypeOf(ForgeOldModMetadata.class));
-                } else {
-                    try {
-                        if (firstToken != JsonToken.BEGIN_OBJECT) {
-                            throw new JsonParseException("Unexpected first token: " + firstToken);
-                        }
-                        ForgeOldModMetadataLst list = (ForgeOldModMetadataLst) JsonUtils.GSON.fromJson(jsonReader, ForgeOldModMetadataLst.class);
-                        if (list == null) {
-                            throw new IOException("Mod " + modFile + " `mcmod.info` is malformed");
-                        }
-                        List<ForgeOldModMetadata> modList2 = list.modList();
-                        modList = modList2;
-                    } catch (Throwable th) {
-                        th = th;
-                        Throwable th2 = th;
-                        try {
-                            jsonReader.close();
-                            throw th2;
-                        } catch (Throwable th3) {
-                            th2.addSuppressed(th3);
-                            throw th2;
-                        }
-                    }
-                }
-                jsonReader.close();
-                if (reader != null) {
-                    reader.close();
-                }
-                if (modList == null || modList.isEmpty()) {
-                    throw new IOException("Mod " + modFile + " `mcmod.info` is malformed");
-                }
-                ForgeOldModMetadata metadata = modList.get(0);
-                String authors2 = metadata.getAuthor();
-                if (StringUtils.isBlank(authors2) && metadata.getAuthors().length > 0) {
-                    authors2 = String.join(", ", metadata.getAuthors());
-                }
-                if (StringUtils.isBlank(authors2) && metadata.getAuthorList().length > 0) {
-                    authors2 = String.join(", ", metadata.getAuthorList());
-                }
-                if (!StringUtils.isBlank(authors2)) {
-                    authors = authors2;
-                } else {
-                    authors = metadata.getCredits();
-                }
-                return new LocalModFile(modManager, modManager.getLocalMod(metadata.getModId(), ModLoaderType.FORGE), modFile, metadata.getName(), new LocalModFile.Description(metadata.getDescription()), authors, metadata.getVersion(), metadata.getGameVersion(), StringUtils.isBlank(metadata.getUrl()) ? metadata.getUpdateUrl() : metadata.url, metadata.getLogoFile());
-            } catch (Throwable th4) {
-                th = th4;
-            }
-        } catch (Throwable th5) {
-            th = th5;
-        }
-        Throwable th6 = th;
-        if (reader == null) {
-            throw th6;
-        }
-        try {
-            reader.close();
-            throw th6;
-        } catch (Throwable th7) {
-            th6.addSuppressed(th7);
-            throw th6;
-        }
+    public static com.brixcore.mod.LocalModFile fromFile(com.brixcore.mod.ModManager r15, java.nio.file.Path r16, java.nio.file.FileSystem r17) throws java.lang.Throwable {
+        /*
+            Method dump skipped, instruction units count: 375
+            To view this dump add '--comments-level debug' option
+        */
+        throw new UnsupportedOperationException("Method not decompiled: com.brixcore.mod.modinfo.ForgeOldModMetadata.fromFile(com.brixcore.mod.ModManager, java.nio.file.Path, java.nio.file.FileSystem):com.brixcore.mod.LocalModFile");
     }
 }
