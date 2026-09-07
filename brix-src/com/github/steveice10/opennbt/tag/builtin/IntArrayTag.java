65e4e1ec2607cd6ea88e7c48c443ff531ced9ac8
wccch <137507230+wccch-wzz@users.noreply.github.com>
2026-09-07 21:52:26 +0800

    sync: Brix 1.1 github/steveice10/opennbt/tag/builtin/IntArrayTag.java

--- brix-src/com/github/steveice10/opennbt/tag/builtin/IntArrayTag.java
+++ brix-src/com/github/steveice10/opennbt/tag/builtin/IntArrayTag.java
@@ -5,6 +5,7 @@ import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import org.apache.commons.lang3.StringUtils;
+import org.lwjgl.system.macosx.ObjCRuntime;
 
 /* JADX INFO: loaded from: classes.dex */
 public class IntArrayTag extends Tag {
@@ -61,7 +62,7 @@ public class IntArrayTag extends Tag {
 
     @Override // com.github.steveice10.opennbt.tag.builtin.Tag
     public void destringify(SNBTIO.StringifiedNBTReader in) throws IOException {
-        String s = in.readUntil(true, ']');
+        String s = in.readUntil(true, ObjCRuntime._C_ARY_E);
         String[] valueStrings = s.substring(s.indexOf(59) + 1, s.length() - 1).replaceAll(StringUtils.SPACE, "").split(",");
         this.value = new int[valueStrings.length];
         for (int i = 0; i < this.value.length; i++) {
@@ -78,13 +79,13 @@ public class IntArrayTag extends Tag {
             sb.append(' ');
         }
         sb.setLength(sb.length() - 2);
-        sb.append(']');
+        sb.append(ObjCRuntime._C_ARY_E);
         out.append((CharSequence) sb.toString());
     }
 
     @Override // com.github.steveice10.opennbt.tag.builtin.Tag
     /* JADX INFO: renamed from: clone */
-    public IntArrayTag mo360clone() {
+    public IntArrayTag mo428clone() {
         return new IntArrayTag(getName(), getValue());
     }
 }
