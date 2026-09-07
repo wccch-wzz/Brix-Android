f5aa066bd9d0582d8937d48ba592120655171b0b
wccch <137507230+wccch-wzz@users.noreply.github.com>
2026-09-07 21:49:44 +0800

    sync: Brix 1.1 android/tools/r8/RecordTag.java

--- brix-src/com/android/tools/r8/RecordTag.java
+++ brix-src/com/android/tools/r8/RecordTag.java
@@ -1,7 +1,7 @@
 package com.android.tools.r8;
 
 /* JADX INFO: compiled from: D8$$SyntheticClass */
-/* JADX INFO: loaded from: classes19.dex */
+/* JADX INFO: loaded from: classes20.dex */
 public abstract /* synthetic */ class RecordTag {
     protected /* synthetic */ RecordTag() {
     }
