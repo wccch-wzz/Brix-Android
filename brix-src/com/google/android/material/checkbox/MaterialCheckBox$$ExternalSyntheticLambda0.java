ec4aa330f5e499a7b8cf490d9311093d87a4018f
wccch <137507230+wccch-wzz@users.noreply.github.com>
2026-09-07 21:53:18 +0800

    sync: Brix 1.1 google/android/material/checkbox/MaterialCheckBox$$ExternalSyntheticLambda0.java

--- brix-src/com/google/android/material/checkbox/MaterialCheckBox$$ExternalSyntheticLambda0.java
+++ brix-src/com/google/android/material/checkbox/MaterialCheckBox$$ExternalSyntheticLambda0.java
@@ -7,6 +7,6 @@ public final /* synthetic */ class MaterialCheckBox$$ExternalSyntheticLambda0 im
 
     @Override // java.lang.Runnable
     public final void run() {
-        this.f$0.m367xdf87d0bf();
+        this.f$0.m435xdf87d0bf();
     }
 }
