b0ab185c2ce8183ef5c9535adbad71dc57fc1c44
wccch <137507230+wccch-wzz@users.noreply.github.com>
2026-09-07 21:49:47 +0800

    sync: Brix 1.1 brix/brixlauncher/BrixAuthActivity.java

--- brix-src/com/brix/brixlauncher/BrixAuthActivity.java
+++ brix-src/com/brix/brixlauncher/BrixAuthActivity.java
@@ -12,7 +12,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import com.brixcore.utils.BrixPath;
 
-/* JADX INFO: loaded from: classes18.dex */
+/* JADX INFO: loaded from: classes19.dex */
 public class BrixAuthActivity extends AppCompatActivity {
     private static final String KEY_AUTH_CODE = "pending_auth_code";
     private static final String PREFS_NAME = "auth_state";
@@ -21,6 +21,8 @@ public class BrixAuthActivity extends AppCompatActivity {
     private AuthManager authManager;
     private Button btnOpenBrowser;
     private String currentAuthCode;
+    private volatile boolean pollingActive;
+    private Thread pollingThread;
 
     @Override // androidx.fragment.app.FragmentActivity, androidx.activity.ComponentActivity, androidx.core.app.ComponentActivity, android.app.Activity
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +32,7 @@ public class BrixAuthActivity extends AppCompatActivity {
                 BrixPath.loadPaths(this);
             }
         } catch (Exception e) {
-            Log.e(TAG, "BrixPath初始化失败: " + e.getMessage());
+            Log.e(TAG, "初始化失败");
         }
         setContentView(R.layout.activity_auth);
         this.authManager = new AuthManager(this);
@@ -46,7 +48,7 @@ public class BrixAuthActivity extends AppCompatActivity {
 
     private void initViews() {
         this.btnOpenBrowser = (Button) findViewById(R.id.btn_open_browser);
-        this.btnOpenBrowser.setOnClickListener(new View.OnClickListener() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda1
+        this.btnOpenBrowser.setOnClickListener(new View.OnClickListener() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda4
             @Override // android.view.View.OnClickListener
             public final void onClick(View view) {
                 this.f$0.lambda$initViews$0(view);
@@ -65,7 +67,7 @@ public class BrixAuthActivity extends AppCompatActivity {
         }
         this.btnOpenBrowser.setEnabled(false);
         this.btnOpenBrowser.setText("正在创建授权码...");
-        new Thread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda2
+        new Thread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda5
             @Override // java.lang.Runnable
             public final void run() {
                 this.f$0.lambda$startAuthFlow$4();
@@ -78,23 +80,23 @@ public class BrixAuthActivity extends AppCompatActivity {
         try {
             this.currentAuthCode = this.authManager.createAuthCode();
             getSharedPreferences(PREFS_NAME, 0).edit().putString(KEY_AUTH_CODE, this.currentAuthCode).apply();
-            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda3
+            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda7
                 @Override // java.lang.Runnable
                 public final void run() {
                     this.f$0.lambda$startAuthFlow$1();
                 }
             });
         } catch (AuthManager.AuthException e) {
-            Log.e(TAG, "授权流程异常: " + e.getMessage());
-            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda4
+            Log.e(TAG, "授权流程异常");
+            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda8
                 @Override // java.lang.Runnable
                 public final void run() {
-                    this.f$0.lambda$startAuthFlow$2(e);
+                    this.f$0.lambda$startAuthFlow$2();
                 }
             });
         } catch (Exception e2) {
-            Log.e(TAG, "未知异常: " + e2.getMessage());
-            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda5
+            Log.e(TAG, "未知异常");
+            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda9
                 @Override // java.lang.Runnable
                 public final void run() {
                     this.f$0.lambda$startAuthFlow$3();
@@ -104,19 +106,12 @@ public class BrixAuthActivity extends AppCompatActivity {
     }
 
     /* JADX INFO: Access modifiers changed from: private */
-    public /* synthetic */ void lambda$startAuthFlow$2(AuthManager.AuthException e) {
+    public /* synthetic */ void lambda$startAuthFlow$2() {
         if (this.btnOpenBrowser != null) {
             this.btnOpenBrowser.setEnabled(true);
             this.btnOpenBrowser.setText("打开浏览器授权");
         }
-        String msg = e.getMessage();
-        if (msg == null || msg.isEmpty()) {
-            Toast.makeText(this, "授权服务暂时不可用，请重试", 0).show();
-        } else if (msg.contains("网络") || msg.contains("超时")) {
-            Toast.makeText(this, "网络连接失败，请检查网络后重试", 0).show();
-        } else {
-            Toast.makeText(this, "授权失败，请重试", 0).show();
-        }
+        Toast.makeText(this, "授权服务暂时不可用，请重试", 0).show();
     }
 
     /* JADX INFO: Access modifiers changed from: private */
@@ -140,6 +135,11 @@ public class BrixAuthActivity extends AppCompatActivity {
     }
 
     private void finishWithSuccess() {
+        this.pollingActive = false;
+        if (this.pollingThread != null) {
+            this.pollingThread.interrupt();
+            this.pollingThread = null;
+        }
         getSharedPreferences(PREFS_NAME, 0).edit().remove(KEY_AUTH_CODE).apply();
         Intent intent = new Intent(this, (Class<?>) BrixLauncher.class);
         intent.setFlags(268468224);
@@ -147,21 +147,91 @@ public class BrixAuthActivity extends AppCompatActivity {
         finish();
     }
 
+    private void startPolling() {
+        if (this.pollingActive) {
+            return;
+        }
+        this.pollingActive = true;
+        this.pollingThread = new Thread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda6
+            @Override // java.lang.Runnable
+            public final void run() {
+                this.f$0.lambda$startPolling$8();
+            }
+        }, "auth-poll");
+        this.pollingThread.setDaemon(true);
+        this.pollingThread.start();
+    }
+
+    /* JADX INFO: Access modifiers changed from: private */
+    public /* synthetic */ void lambda$startPolling$8() {
+        try {
+            this.authManager.pollUntilAuthorized();
+            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda0
+                @Override // java.lang.Runnable
+                public final void run() {
+                    this.f$0.lambda$startPolling$5();
+                }
+            });
+        } catch (AuthManager.AuthException e) {
+            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda1
+                @Override // java.lang.Runnable
+                public final void run() {
+                    this.f$0.lambda$startPolling$6();
+                }
+            });
+        } catch (Exception e2) {
+            runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda2
+                @Override // java.lang.Runnable
+                public final void run() {
+                    this.f$0.lambda$startPolling$7();
+                }
+            });
+        }
+    }
+
+    /* JADX INFO: Access modifiers changed from: private */
+    public /* synthetic */ void lambda$startPolling$5() {
+        if (this.pollingActive) {
+            finishWithSuccess();
+        }
+    }
+
+    /* JADX INFO: Access modifiers changed from: private */
+    public /* synthetic */ void lambda$startPolling$6() {
+        Toast.makeText(this, "授权超时，请重新点击按钮", 0).show();
+        if (this.btnOpenBrowser != null) {
+            this.btnOpenBrowser.setEnabled(true);
+            this.btnOpenBrowser.setText("打开浏览器授权");
+        }
+    }
+
+    /* JADX INFO: Access modifiers changed from: private */
+    public /* synthetic */ void lambda$startPolling$7() {
+        Toast.makeText(this, "授权检查失败，请重试", 0).show();
+        if (this.btnOpenBrowser != null) {
+            this.btnOpenBrowser.setEnabled(true);
+            this.btnOpenBrowser.setText("打开浏览器授权");
+        }
+    }
+
     @Override // androidx.fragment.app.FragmentActivity, android.app.Activity
     protected void onResume() {
         super.onResume();
+        if (this.currentAuthCode != null && !this.authManager.isLoggedIn()) {
+            startPolling();
+        }
         checkAuthResult();
     }
 
     private void checkAuthResult() {
         try {
-            if (!this.authManager.isLoggedIn() || this.currentAuthCode == null) {
+            if (!this.authManager.isLoggedIn()) {
                 final String error = getSharedPreferences(PREFS_NAME, 0).getString("auth_error", null);
                 if (error != null && !error.isEmpty()) {
-                    runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda0
+                    runOnUiThread(new Runnable() { // from class: com.brix.brixlauncher.BrixAuthActivity$$ExternalSyntheticLambda3
                         @Override // java.lang.Runnable
                         public final void run() {
-                            this.f$0.lambda$checkAuthResult$5(error);
+                            this.f$0.lambda$checkAuthResult$9(error);
                         }
                     });
                     getSharedPreferences(PREFS_NAME, 0).edit().remove("auth_error").apply();
@@ -169,15 +239,15 @@ public class BrixAuthActivity extends AppCompatActivity {
                 }
                 return;
             }
-            Log.d(TAG, "授权成功，返回主界面");
+            Log.d(TAG, "已登录，进入主界面");
             finishWithSuccess();
         } catch (Exception e) {
-            Log.e(TAG, "检查授权结果异常: " + e.getMessage());
+            Log.e(TAG, "检查授权结果异常");
         }
     }
 
     /* JADX INFO: Access modifiers changed from: private */
-    public /* synthetic */ void lambda$checkAuthResult$5(String error) {
+    public /* synthetic */ void lambda$checkAuthResult$9(String error) {
         if (this.btnOpenBrowser != null) {
             this.btnOpenBrowser.setEnabled(true);
             this.btnOpenBrowser.setText("打开浏览器授权");
